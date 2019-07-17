a8bbed35ad65cf4d94dfe2d268c401e2d5f29db4
taisa <g5.taisa831@gmail.com>
2019-07-17 17:16:06 +0900

    event handling

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,117 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+    <div class="hello">
+        <h1>{{ msg }}</h1>
+
+        <h1>メソッドイベントハンドラ</h1>
+        <button v-on:click="handleClick">クリック</button><br>
+
+        <h1>インラインイベントハンドラ</h1>
+        <button v-on:click="count++">クリック</button><br>
+        <button v-on:click="handleClick2($event, msg)">クリック</button><br>
+
+        <h1>使用可能なイベント</h1>
+        <img v-bind:src="'./logo.png'" v-on:load="show=true" v-bind:class="{hide: !show}">
+        <div v-on:dragstart="handler" draggable="true" id="hoge">ドラッグ可能</div>
+
+        <h1>フォーム入力の取得</h1>
+        <input v-bind:value="message" v-on:input="handleInput">
+
+        <h1>イベント修飾子</h1>
+        <p>.right</p>
+        <div @click.right="handler" id="right">example</div>
+        <!-- 右クリック時のコンテキストメニューを表示させない -->
+        <div @click.right.prevent="handler" id="right-prevent">example</div>
+
+        <p>.stop</p>
+        <div @click="print('div1')">
+            div1
+            <a href="#top" @click.stop="print('div2')">div2</a>
+        </div>
+
+        <p>.prevent</p>
+        <div @click="print('div1')">
+            div1
+            <a href="#top" @click.prevent="print('div2')">div2</a>
+        </div>
+
+        <p>.capture</p>
+        <div @click.capture="print('div1')">
+            div1
+            <div @click="print('div2')">
+                div2
+                <div @click="print('div3')">
+                    div3
+                </div>
+            </div>
+        </div>
+
+    </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+    export default {
+        name: 'HelloWorld',
+        props: {
+            msg: String,
+        },
+        data() {
+            return {
+                show: false,
+                message: 'Hello!',
+                count: 0
+            }
+        },
+        methods: {
+            handleClick() {
+                this.count++
+            },
+            handleClick2(event, msg) {
+                console.log(event)
+                console.log(msg)
+                this.message = msg
+            },
+            handler(event) {
+                //event.dataTransfer.setData("text", event.target.id)
+                console.log(event.target.id)
+            },
+            handleInput(event) {
+                console.log(event.target.value)
+                this.message = event.target.value
+            },
+            print(str) {
+                console.log(str)
+            }
+        }
+    }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+    h3 {
+        margin: 40px 0 0;
+    }
+
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
+
+    li {
+        display: inline-block;
+        margin: 0 10px;
+    }
+
+    a {
+        color: #42b983;
+    }
+
+    img {
+        opacity: 1;
+        transition: opacity 1s;
+    }
+
+    img.hide {
+        opacity: 0;
+    }
+
 </style>
