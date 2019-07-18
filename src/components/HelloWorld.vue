a3a419e4bc96f3da1787fbf025fbb505792bb5da
taisa <g5.taisa831@gmail.com>
2019-07-18 19:33:05 +0900

    マウント要素外のイベントと操作

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,11 +5,14 @@
         <h1>イベントハンドラ</h1>
 
         <h2>メソッドイベントハンドラ</h2>
-        <button v-on:click="handleClick">クリック</button><br>
+        <button v-on:click="handleClick">クリック</button>
+        <br>
 
         <h2>インラインイベントハンドラ</h2>
-        <button v-on:click="count++">クリック</button><br>
-        <button v-on:click="handleClick2($event, msg)">クリック</button><br>
+        <button v-on:click="count++">クリック</button>
+        <br>
+        <button v-on:click="handleClick2($event, msg)">クリック</button>
+        <br>
 
         <h2>使用可能なイベント</h2>
         <img v-bind:src="'./logo.png'" v-on:load="show=true" v-bind:class="{hide: !show}">
@@ -106,59 +109,85 @@
         <pre>{{text}}</pre>
 
         <h2>マウント要素外のイベントと操作</h2>
+        <header v-bind:class="{ compact: scrollY > 200}">
+            200pxより下にスクロールしたら .compact を付与
+        </header>
+
+        <div @click="scrollTop">
+            ページ上部へ移動
+        </div>
     </div>
 </template>
 
 <script>
-    export default {
-        name: 'HelloWorld',
-        props: {
-            msg: String,
-        },
-        data() {
-            return {
-                show: false,
-                message: 'Hello!',
-                count: 0,
-                val: false,
-                multiVal: [],
-                radioVal: '',
-                selectVal: '',
-                selectMultiVal: [],
-                preview: '',
-                rangeVal: 50,
-                price: 1000,
-                text: ''
-            }
-        },
-        methods: {
-            handleClick() {
-                this.count++
-            },
-            handleClick2(event, msg) {
-                console.log(event)
-                console.log(msg)
-                this.message = msg
-            },
-            handler(event) {
-                //event.dataTransfer.setData("text", event.target.id)
-                console.log(event.target.id)
-            },
-            handleInput(event) {
-                console.log(event.target.value)
-                this.message = event.target.value
-            },
-            print(str) {
-                console.log(str)
-            },
-            fileChange(event) {
-                let file = event.target.files[0]
-                if (file) {
-                    this.preview = window.URL.createObjectURL(file)
-                }
-            }
+
+  export default {
+    name: 'HelloWorld',
+    props: {
+      msg: String,
+    },
+    data() {
+      return {
+        show: false,
+        message: 'Hello!',
+        count: 0,
+        val: false,
+        multiVal: [],
+        radioVal: '',
+        selectVal: '',
+        selectMultiVal: [],
+        preview: '',
+        rangeVal: 50,
+        price: 1000,
+        text: '',
+        scrollY: 0,
+        timer: null,
+        scroll: {}
+      }
+    },
+    created() {
+      window.addEventListener('scroll', this.handleScroll)
+    },
+    methods: {
+      handleClick() {
+        this.count++
+      },
+      handleClick2(event, msg) {
+        console.log(event)
+        console.log(msg)
+        this.message = msg
+      },
+      handler(event) {
+        //event.dataTransfer.setData("text", event.target.id)
+        console.log(event.target.id)
+      },
+      handleInput(event) {
+        console.log(event.target.value)
+        this.message = event.target.value
+      },
+      print(str) {
+        console.log(str)
+      },
+      fileChange(event) {
+        let file = event.target.files[0]
+        if (file) {
+          this.preview = window.URL.createObjectURL(file)
         }
+      },
+      handleScroll() {
+        if (this.timer === null) {
+          this.timer = setTimeout(function() {
+            this.scrollY = window.scrollY
+            clearTimeout(this.timer)
+            this.timer = null
+          }.bind(this), 200)
+        }
+      },
+      scrollTop() {
+        this.$SmoothScroll(document.querySelector('#app'), 400, null, null, 'y')
+      }
     }
+  }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import smoothScroll from "vue-smoothscroll";
+
 Vue.config.productionTip = false
+Vue.use(smoothScroll)
 
 new Vue({
   render: h => h(App),
